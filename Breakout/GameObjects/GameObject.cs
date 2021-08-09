2ae4d1bb527a4856662795138352d30ab52b0534
Aardhyn Lavender (1000096681) <LAVEAT1@student.op.ac.nz>
2021-08-10 11:59:24 +1200

    defined basic methods and members to defined a generic game object

---
+++ Breakout/GameObjects/GameObject.cs
@@ -0,0 +1,80 @@
+﻿
+//
+//  GameObject class
+//
+//  A generic representation of a object in 2d space that can be updated and rendered to the screen.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Breakout.Utility;
+
+namespace Breakout.GameObjects
+{
+    class GameObject
+    {
+        // postion and size
+        private float x;
+        private float y;
+        private float width;
+        private float height;
+
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        // texture
+        private Image texture;
+        private int sourceX;
+        private int sourceY;
+        private int sourceWidth;
+        private int sourceHeight;
+        
+        public Image Texture { get; set; }
+
+        // physics
+        private Vector2D velocity;
+        private bool ghost;
+
+        public GameObject(float x, float y, Image texture, bool ghost = false)
+        {
+            this.x = x;
+            this.y = y;
+            this.texture = texture;
+
+            width = texture.Width;
+            height = texture.Height;
+
+            velocity.Zero();
+        }
+
+        public GameObject(float x, float y, Image texture, int sourceWidth, int sourceHeight, bool ghost = false)
+        {
+            this.x = x;
+            this.y = y;
+            this.texture = texture;
+
+            width = sourceWidth;
+            height = sourceHeight;
+
+            velocity.Zero();
+        }
+
+
+        public virtual void Update()
+        {
+            x += velocity.X;
+            y += velocity.Y;
+        }
+
+        public virtual void OnDestory()
+        {  }
+
+        public virtual void OnCollsion(GameObject collider)
+        {  }
+    }
+}
