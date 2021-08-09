3b9bb9e7f41a6b01774347ae34d3658b6b9672c4
Aardhyn Lavender (1000096681) <LAVEAT1@student.op.ac.nz>
2021-08-10 11:58:56 +1200

    declared basic methods and members for an abstract game

---
+++ Breakout/Game.cs
@@ -0,0 +1,60 @@
+﻿
+//
+//
+//
+//
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Breakout.Render;
+using Breakout.GameObjects;
+
+namespace Breakout
+{
+    abstract class Game
+    {
+        private Screen screen;
+
+        private int width;
+        private int height;
+        private int scale;
+
+        private List<GameObject> gameObjects;
+
+        public Game(Screen screen)
+        {
+            this.screen = screen;
+            StartGame();
+        }
+
+        public virtual void Update()
+        {
+            foreach (GameObject gameObject in gameObjects) gameObject.Update();
+        }
+
+        public virtual void PhysicsProcess()
+        {
+
+        }
+
+        public virtual void Render()
+        {
+            screen.RenderClear();
+
+            foreach (GameObject gameObject in gameObjects)
+                screen.Buffer.DrawImage(gameObject.Texture, gameObject.X, gameObject.Y);
+
+            screen.RenderPresent();
+        }
+
+        public abstract void GameLoop();
+        public abstract void StartGame();
+        public abstract void EndGame();
+        public abstract void SaveGame();
+    }
+}
