66019628122385332fb41e86b609e17460b59c3d
Aardhyn Lavender (1000096681) <LAVEAT1@student.op.ac.nz>
2021-08-10 11:59:51 +1200

    Created a screen class to manage rendering to the screen outside the form

---
+++ Breakout/Render/Screen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Breakout.Render
+{
+    public sealed class Screen
+    {
+        public Graphics Buffer;
+
+        private Image BufferImage;
+        private Graphics Display;
+
+        public int Width;
+        public int Height; 
+
+        public Screen(Graphics display)
+        {
+            BufferImage = new Bitmap(Width, Height);
+            Buffer = Graphics.FromImage(BufferImage);
+            Display = display;
+        }
+
+        public void RenderClear()
+            => Buffer.FillRectangle(Brushes.Black, new Rectangle(0, 0, Width, Height));
+
+        public void RenderPresent()
+            => Display.DrawImage(BufferImage, 0, 0);
+    }
+}
