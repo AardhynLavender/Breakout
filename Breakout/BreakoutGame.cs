d72e3f600df17c6bb102a3f8ad9ade1aef72510f
Aardhynlavneder <laveat1@student.op.ac.nz>
2021-08-10 16:37:00 +1200

    testing rendering

--- Breakout/BreakoutGame.cs
+++ Breakout/BreakoutGame.cs
@@ -5,16 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Breakout.Render;
+using Breakout.GameObjects;
 
 namespace Breakout
 {
     class BreakoutGame : Game
     {
+        const int SCALE = 4;
 
         public BreakoutGame(Screen screen) 
             : base(screen)
         {
-            
+            screen.Scale = SCALE;
+
+            AddGameObject(new GameObject(0, 0, Properties.Resources.dirt));
         }
 
         public override void EndGame()
