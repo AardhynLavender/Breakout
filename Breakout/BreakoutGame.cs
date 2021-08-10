2edb29d0aeb576b77a27ad13c8dd3f7ec9e05cde
Aardhyn Lavender (1000096681) <LAVEAT1@student.op.ac.nz>
2021-08-10 12:26:13 +1200

    created derived game class object

---
+++ Breakout/BreakoutGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Breakout.Render;
+
+namespace Breakout
+{
+    class BreakoutGame : Game
+    {
+
+        public BreakoutGame(Screen screen) 
+            : base(screen)
+        {
+            
+        }
+
+        public override void EndGame()
+        {
+            
+        }
+
+        public override void GameLoop()
+        {
+            Update();
+            Render();
+            tick++;
+        }
+
+        public override void SaveGame()
+        {
+            
+        }
+
+        public override void StartGame()
+        {
+            
+        }
+    }
+}
