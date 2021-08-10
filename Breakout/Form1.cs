b1a11921d63903de4620177f9556f7c5d1211b26
Aardhyn Lavender (1000096681) <LAVEAT1@student.op.ac.nz>
2021-08-10 12:00:23 +1200

    form design

--- Breakout/Form1.cs
+++ Breakout/Form1.cs
@@ -8,13 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Breakout.Render;
+
 namespace Breakout
 {
     public partial class Form1 : Form
     {
+        Game breakout;
+
         public Form1()
         {
             InitializeComponent();
+            //breakout = new Game(new Render.Screen(CreateGraphics()));
         }
     }
 }
