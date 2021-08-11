d3572f535cc604b1b6ebb2b623a47a9ca3cdd1a8
Aardhyn Lavender (1000096681) <LAVEAT1@student.op.ac.nz>
2021-08-11 12:00:02 +1200

    added basic ball class

---
+++ Breakout/GameObjects/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Breakout;
+
+namespace Breakout.GameObjects
+{
+    class Ball : GameObject
+    {
+        private float angle;
+        private float magnitude;
+
+        public Ball(int x, int y, float angle, float magnitude)
+            : base(x, y, BreakoutGame.assets.Texture, BreakoutGame.assets.GetTile(0))
+        {
+            this.angle = angle;
+            this.magnitude = magnitude;
+        }
+    }
+}
